b98595f561604759c360796fbf8f717ed01d04c9
Piotrek <@>
2017-10-25 18:42:32 +0200

    pierwszy commit

---
+++ ConsoleApplication24/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+
+        private Car()
+        {
+            marka = "Skoda";
+        }
+
+        private Car(double pojemonoscSilnika, string marka)
+        {
+            this.marka = marka;
+            this.pojemnoscSilnika = pojemonoscSilnika;
+
+        }
+
+        public static Car Create(double poj, string marka)
+        {
+            Car temp = new Car(poj, marka);
+            return temp;
+        }
+
+        public string Info()
+        {
+            return marka + pojemnoscSilnika;
+        }
+
+        public readonly static int iloscKol;
+
+        static Car()
+        {
+            iloscKol = 4;
+        }
+
+        //~Car()
+        //{
+        //    Console.WriteLine("Zwalniam pamięć");
+        //    Console.ReadKey();
+        //}
+
+        public const int iloscDrzwi = 4;
+    }
+}
---
+++ ConsoleApplication24/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car auto = new Car();
+
+            //Car auto2 = new Car(45.6,"Ford");
+
+            Car auto3 = Car.Create(3.4, "Fiat");
+            //Car.iloscKol = 5;
+            Car auto4 = Car.Create(4.2, "Opel");
+            //Console.WriteLine(auto3.Info());
+            //auto3.iloscDrzwi = 4;
+            //Console.ReadKey();
+            
+        }
+    }
+}
